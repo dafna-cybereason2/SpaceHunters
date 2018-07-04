54de0219184d0c965e7559edaa1bf9b18a3557c1
alona <alona@cybereason.com>
2018-07-04 14:59:45 +0300

    part 7 phase 2

--- 2DSpaceShooter/Assets/Scripts/GameManager.cs
+++ 2DSpaceShooter/Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    //Game objects
+    public GameObject playButton;
+    public GameObject playerShip;
+
     public enum GameManagerState
     {
         Opening,
@@ -11,15 +16,45 @@ public class GameManager : MonoBehaviour
         GameOver,
     }
 
+    GameManagerState GMState;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        GMState = GameManagerState.Opening;
 	}
 	
-	// Update is called once per frame
-	void Update ()
+	// Update game manager state
+	void UpdateGameManagerState ()
     {
-		
+        switch(GMState)
+        {
+            case GameManagerState.Opening:
+                break;
+            case GameManagerState.Gameplay:
+                //hide play button
+                playButton.SetActive(false);
+
+                //set player ship visible
+                playerShip.GetComponent<PlayerControl>().Init();
+
+                break;
+            case GameManagerState.GameOver:
+                break;
+        }
 	}
+
+    //Set game manager state
+    public void SetGameManagerState(GameManagerState state)
+    {
+        GMState = state;
+        UpdateGameManagerState();
+    }
+    
+    //will be called when user clicks play
+    public void StartGamePlay()
+    {
+        GMState = GameManagerState.Gameplay;
+        UpdateGameManagerState();
+    }
 }
