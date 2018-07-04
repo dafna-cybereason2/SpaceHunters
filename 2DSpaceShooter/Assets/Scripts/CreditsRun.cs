475cf8de5bace6a97e2fa47ed2af164a4ed155a3
alona <alona@cybereason.com>
2018-07-04 22:57:02 +0300

    credits scene

---
+++ 2DSpaceShooter/Assets/Scripts/CreditsRun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsRun : MonoBehaviour
+{
+    public float y0ffset = 1f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        new WaitForSecondsRealtime(6f);
+        SceneManager.LoadScene("SampleScene");
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+   { 
+         transform.Translate(Vector2.up * y0ffset * Time.deltaTime); 
+       
+    }
+}
