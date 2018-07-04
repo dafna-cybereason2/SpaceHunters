09a0b6ec015876cbf18f1137d464587a0355b603
Linn.Tamar-Omer <linn@cybereason.com>
2018-07-04 23:28:15 +0300

    menu

---
+++ 2DSpaceShooter/Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsController : MonoBehaviour
+{
+    int index = 0;
+    public int totalLevels = 4;
+    public float y0ffset = 100f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal"); // -1/0/1 = left , no input, right
+        float y = Input.GetAxisRaw("Vertical");
+
+        if ( y == -1 )
+        {
+            if (index < totalLevels-1)
+            {
+                index++;
+                Vector2 position = transform.position;
+                position.y -= y0ffset;
+                transform.position = position;
+                Debug.Log(transform.position);
+                
+            }
+        }
+
+        if (y == 1)
+        {
+            if (index >= 1)
+            {
+                index--;
+                Vector2 position = transform.position;
+                position.y += y0ffset;
+                transform.position = position;
+                Debug.Log(transform.position);
+            }
+        }
+
+    }
+}
