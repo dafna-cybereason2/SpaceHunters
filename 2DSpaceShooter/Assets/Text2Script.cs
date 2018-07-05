65d0e9b893bb273a5a462d461cd84d4a001bd858
kobi malul <kobi@cyberdomain.local>
2018-07-05 14:12:42 +0300

    no lives on single player

---
+++ 2DSpaceShooter/Assets/Text2Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text2Script : MonoBehaviour {
+
+	public GameObject GameData;
+
+	// Use this for initialization
+	void Start () {
+
+		GameData = GameObject.FindGameObjectWithTag ("GameData");
+		if (GameData.GetComponent<GameDataControl> ().TwoPlayerGame == false) {
+			gameObject.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
